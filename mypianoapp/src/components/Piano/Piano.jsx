356687a41d018a42a25c0c1944cc4d66f0b88086
Lorena Carmona <126011228+Ananke85@users.noreply.github.com>
2023-08-21 17:17:45 +0200

    FEAT(Keys): Added sound to keys

--- mypianoapp/src/components/Piano/Piano.jsx
+++ mypianoapp/src/components/Piano/Piano.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import styles from './piano.module.css';
-import WhiteKeys from '../WhiteKeys/whiteKeys';
-import BlackKeys from '../BlackKeys/BlackKeys';
-import Keyboard from '../Keyboard/Keyboard';
+import React, { useRef, useState } from "react";
+import styles from "./piano.module.css";
+import WhiteKeys from "../WhiteKeys/whiteKeys";
+import BlackKeys from "../BlackKeys/BlackKeys";
+import Keyboard from "../Keyboard/Keyboard";
 
 const Piano = () => {
-    return (
-        <div className={styles.piano}>
-            {/* <h2>Play with music</h2> */}
-            <Keyboard />
-            <WhiteKeys />
-            <BlackKeys />
-            
-        </div>
-    );
-};
+  const audioRef = useRef(null);
+  const playAudio = (sound) => {
+    audioRef.current.src = sound;
+    audioRef.current.play();
+  };
 
+  return (
+    <div className={styles.piano}>
+      {/* <h2>Play with music</h2> */}
+      <Keyboard />
+      <WhiteKeys playAudio={playAudio} />
+      <BlackKeys playAudio={playAudio} />
+      <audio ref={audioRef} />
+    </div>
+  );
+};
 
 export default Piano;
--- mypianoapp/src/components/WhiteKeys/WhiteKeys.jsx
+++ mypianoapp/src/components/WhiteKeys/WhiteKeys.jsx
@@ -1,25 +1,40 @@
-import { useState } from 'react';
-import styles from './whiteKeys.module.css';
+import { useState } from "react";
+import styles from "./whiteKeys.module.css";
+import do_sound from "../../assets/notes/do.wav";
+import re from "../../assets/notes/re.wav";
+import mi from "../../assets/notes/mi.wav";
+import fa from "../../assets/notes/fa.wav";
+import sol from "../../assets/notes/sol.wav";
+import la from "../../assets/notes/la.wav";
+import si from "../../assets/notes/si.wav";
 
-const WhiteKeys = () => {
-    const whiteNotes = ['DO', 'RE', 'MI', 'FA', 'SOL', 'LA', 'SI'];
-    
-    return (
-        <div className={styles.whiteKeys}>
+const WhiteKeys = ({ playAudio }) => {
+  const whiteNotes = ["DO", "RE", "MI", "FA", "SOL", "LA", "SI"];
+  const whiteSounds = {
+    DO: do_sound,
+    RE: re,
+    MI: mi,
+    FA: fa,
+    SOL: sol,
+    LA: la,
+    SI: si,
+  };
 
-            {whiteNotes.map((note) => (
-                <button key={note}>{note}</button>
-            ))}
-            {/* <button>DO</button>
-            <button>RE</button>
-            <button>MI</button>
-            <button>FA</button>
-            <button>SOL</button>
-            <button>LA</button>
-            <button>SI</button> */}
-        
-        </div>
-    );
+  console.log("los sonidos", whiteSounds);
+  const handleNoteClick = (note) => {
+    console.log("tocando");
+    playAudio(whiteSounds[note]);
+  };
+
+  return (
+    <div className={styles.whiteKeys}>
+      {whiteNotes.map((note) => (
+        <button key={note} onClick={() => handleNoteClick(note)}>
+          {note}
+        </button>
+      ))}
+    </div>
+  );
 };
 
 export default WhiteKeys;
